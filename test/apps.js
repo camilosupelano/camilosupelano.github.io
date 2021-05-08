688f33f5e88acee350fa43909d255a4e36c8ebd9
camilosupelano <sup16001@byui.edu>
2021-05-07 22:46:52 -0500

    commit

---
+++ test/apps.js
@@ -0,0 +1,70 @@
+// kind of data
+"Hello world" // string
+'Hello World' // string
+
+123456 -2.3 // number
+
+//boolean
+true 
+false
+
+// array
+['joe', 'ryan', 'martha']
+[1,2,3]
+[true, false, true, false]
+
+// object
+
+
+{
+    "username": "ryan",
+    "score": 70.4,
+    "hours": 14,
+    "professional": true
+    "friends": ["ana", "Camilo", "Cristian"]
+}
+
+console.log("this is a string")
+
+// Variables
+
+var username = "Camilo";
+let latname = "Supelano";
+
+username = "Eddie";
+
+console.log(username)
+
+// Constante
+
+const PI = 3.1415;
+const ID = 2421292
+
+// camelcase
+
+let full_name = "Camilo Supelano"
+let fullName 
+
+///Functions
+
+let password = "Camilo123"
+
+let input = "camila123"
+
+let result = input == password;
+
+if (result === true) {
+    console.log("correct login");
+}
+
+if (result === false) {console.log("incorrect login")};
+
+console.log(result)
+
+// same but shorter 
+
+if (result === true) {
+    console.log("correct login");
+} else {console.log("incorrect passwotd");}
+
+
