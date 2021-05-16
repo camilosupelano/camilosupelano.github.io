564e56b9d6c4a7118c7f739c8b1585349a243b77
camilosupelano <sup16001@byui.edu>
2021-05-15 22:32:39 -0500

    Commit

---
+++ W4/js/scripture.js
@@ -0,0 +1,15 @@
+/*
+ * Input: A person's name
+ * Processing: Create a verse of scripture for that person
+ * Output: The scripture
+ */
+
+function addName() {
+
+    let name = document.getElementById('nameInputBox').value;
+	//
+    let scripture = "And it came to pass that I," + name + ", said unto my father: I will go and do the things which the Lord hath commanded, for I know that the Lord giveth no commandments unto the children of men, save he shall prepare a way for them that they may accomplish the thing which he commandeth them.";
+
+    document.getElementById('output2').innerHTML = scripture;
+
+}
