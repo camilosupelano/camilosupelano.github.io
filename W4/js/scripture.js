44518b751ac98a7787a67508d7dfaf377396920d
camilosupelano <sup16001@byui.edu>
2021-05-28 00:28:17 -0500

    Commit

--- W4/js/scripture.js
+++ W4/js/scripture.js
@@ -8,7 +8,7 @@ function addname() {
 
     let name = document.getElementById('nameInputBox').value;
 	//
-    let scripture = "And it came to pass that I," + name + ", said unto my father: I will go and do the things which the Lord hath commanded, for I know that the Lord giveth no commandments unto the children of men, save he shall prepare a way for them that they may accomplish the thing which he commandeth them.";
+    let scripture = "And it came to pass that I, " + name + ", said unto my father: I will go and do the things which the Lord hath commanded, for I know that the Lord giveth no commandments unto the children of men, save he shall prepare a way for them that they may accomplish the thing which he commandeth them.";
 
     document.getElementById('output2').innerHTML = scripture;
 
