1421e5b91f99347aae541f3aa8bcca83356f776d
camilosupelano <sup16001@byui.edu>
2021-06-05 00:50:19 -0500

    commit

---
+++ w7/js/discount.js
@@ -0,0 +1,28 @@
+
+    //Increase the sales on Tuesdays and Wednesday
+    //if subtotal > $50 -10% off
+    //+ 6% tax
+
+    function total() {
+        //INPUT the amounts, days of the week
+    
+        let subtotal = parseFloat(document.getElementById('subtotal').value);
+        let dayOfWeek = new Date().getDay();
+        let days = dayOfWeek = 2 | 3;
+        let total = 0;
+    
+    
+        //PROCESSING calculation according to conditionals
+         
+        if (subtotal > 50 && days){
+            total = (subtotal + (subtotal*.06))-(subtotal + (subtotal*.06))*0.1
+        } else {total = subtotal + (subtotal*.06)
+    
+        }
+        //OUTPUT the amount with the applied discount and the tax
+
+         document.getElementById('output').innerHTML = "$" + total.toFixed(2);
+
+        }
+    
+    
